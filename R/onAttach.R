8bfa24082ad064b552101221c65286ceebcce2e3
Richard White <a@rwhite.no>
2017-07-30 12:05:43 +0200

    Versioning

---
+++ R/onAttach.R
@@ -0,0 +1,5 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage('PACKAGE: reporttb')
+  packageStartupMessage('Version')
+  packageStartupMessage('Developed by Richard White, Norwegian Institute of Public Health')
+}
